994ba8dd13d75de81b9fbccd385cd3a21b38960f
Gideon Funom Bature <infoaboutgideon@gmail.com>
2022-09-22 05:02:11 +0100

    modify playRound Function, and add game function

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
 function getComputerChoice() {
   let compGuess = Math.floor(Math.random() * 3 + 1);
   if (compGuess == 1) {
@@ -13,24 +9,34 @@ function getComputerChoice() {
   }
 }
 
-function playRound(computerSelection, playerSelection) {
-  if (computerSelection == "rock" && playerSelection == "paper") {
-    return "player wins";
-  } else if (computerSelection == "rock" && playerSelection == "scissors") {
-    return "computer wins";
-  } else if (computerSelection == "paper" && playerSelection == "rock") {
-    return "computer wins";
-  } else if (computerSelection == "paper" && playerSelection == "scissors") {
-    return "player wins";
-  } else if (computerSelection == "scissors" && playerSelection == "rock") {
-    return "player wins";
-  } else if (computerSelection == "scissors" && playerSelection == "paper") {
-    return "computer wins";
+function playRound(playerSelection, computerSelection) {
+  if (
+    (computerSelection == "rock" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "rock")
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "paper" && playerSelection == "rock") ||
+    (computerSelection == "scissors" && playerSelection == "paper")
+  ) {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else {
-    return "it is a draw";
+    return `It is a Draw, you choose ${playerSelection}`;
   }
 }
-let computerSelection = getComputerChoice();
-let playerInput = prompt("Please Enter Rock, Paper or Scissors:");
-let playerSelection = playerInput.toLowerCase();
-console.log(playRound(playerSelection, computerSelection));
+
+const playerSelection = prompt(
+  "Please Enter Rock, Paper or Scissors:"
+).toLowerCase();
+const computerSelection = getComputerChoice();
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    // your code here!
+    return playRound(playerSelection, computerSelection);
+  }
+}
+
+console.log(game());
