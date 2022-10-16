3ea73f4d84c3a87536383eb55b39ef25427ae382
GideonBature <infoaboutgideon@gmail.com>
2022-10-17 00:46:17 +0100

    add html, css and JavaScript file

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-function getComputerChoice() {
-  let compGuess = Math.floor(Math.random() * 3 + 1);
-  if (compGuess == 1) {
-    return "rock";
-  } else if (compGuess == 2) {
-    return "paper";
-  } else {
-    return "scissors";
+const options = document.querySelector('.btn');
+const winner = document.querySelector('.winner');
+const pString = document.querySelector('.pString');
+const cString = document.querySelector('.cString');
+
+let playerCount = 0;
+let computerCount = 0;
+
+options.addEventListener('click', (e) => {
+
+   const playerSelection = e.target.dataset.set;
+   pString.textContent = playerSelection;
+
+    function getComputerChoice() {
+    let compGuess = Math.floor(Math.random() * 3 + 1);
+    if (compGuess == 1) {
+      return "rock";
+    } else if (compGuess == 2) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
   }
-}
+  const computerSelection = getComputerChoice();
+  cString.textContent = computerSelection;
+
+  
+  
+  playRound(playerSelection, computerSelection)
+  
+  updateScore();
+
+  gameWinner();
+
+}) 
+
 
 function playRound(playerSelection, computerSelection) {
   if (
@@ -15,28 +41,32 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection == "paper" && playerSelection == "scissors") ||
     (computerSelection == "scissors" && playerSelection == "rock")
   ) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    return (`You Win! ${playerSelection} beats ${computerSelection}`, playerCount++);
   } else if (
     (computerSelection == "rock" && playerSelection == "scissors") ||
     (computerSelection == "paper" && playerSelection == "rock") ||
     (computerSelection == "scissors" && playerSelection == "paper")
   ) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return (`You Lose! ${computerSelection} beats ${playerSelection}`, computerCount++);
   } else {
     return `It is a Draw, you choose ${playerSelection}`;
   }
 }
 
-const playerSelection = prompt(
-  "Please Enter Rock, Paper or Scissors:"
-).toLowerCase();
-const computerSelection = getComputerChoice();
+function updateScore() {
+  document.getElementById('pp').textContent = playerCount;
+  document.getElementById('cp').textContent = computerCount;
+}
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    // your code here!
-    return playRound(playerSelection, computerSelection);
+function gameWinner() {
+  if (playerCount == 5 || computerCount == 5) {
+    if (playerCount > computerCount) {
+      winner.textContent = "You!";
+      winner.style.color = "brown";
+    } else {
+      winner.textContent = "Computer!";
+      winner.style.color = "brown";
+    }
   }
 }
 
-console.log(game());
